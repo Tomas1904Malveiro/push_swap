eca3ae583fc2d165ea71b1c3a5366624fc79bd6a
Tomas1904Malveiro <148541225+Tomas1904Malveiro@users.noreply.github.com>
2026-05-19 13:56:01 +0100

    Add files via upload

--- stack_init.c
+++ stack_init.c
@@ -6,7 +6,7 @@
 /*   By: tochaves <tochaves@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/18 13:40:21 by tochaves          #+#    #+#             */
-/*   Updated: 2026/05/18 15:58:43 by tochaves         ###   ########.fr       */
+/*   Updated: 2026/05/19 13:43:02 by tochaves         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,36 @@ t_stack	*stack_new(int size)
 	s = malloc(sizeof(t_stack));
 	if (!s)
 		return (NULL);
+	s->data = malloc(sizeof(int) * size);
+	if (!s->data)
+	{
+		free(s);
+		return (NULL);
+	}
+	s->size = size;
+	s->top = -1;
 	return (s);
 }
 
 void	stack_free(t_stack *s)
 {
+	if (!s)
+		return ;
 	free(s->data);
 	free(s);
 }
 
-int		stack_is_sorted(t_stack *s);
+int	stack_is_sorted(t_stack *s)
+{
+	int i;
+	i = 0;
+	if (s->top == -1)
+		return (1);
+	while (i < s->top)
+	{
+		if (s->data[i] > s->data[i + 1])
+			return (0);
+		i++;
+	}
+	return (1);
+}
