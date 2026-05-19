646e94162e492fe530303afc144b2c42e661ec6a
Bootsole <kayansb@gmail.com>
2026-05-19 15:31:37 +0100

    Add files via upload

---
+++ compute_disorder.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   compute_disorder.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbezerra <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/19 14:34:27 by kbezerra          #+#    #+#             */
+/*   Updated: 2026/05/19 14:34:29 by kbezerra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+long	compute_disorder(t_stack *a)
+{
+	long	mistakes;
+	long	total_pairs;
+	long	i;
+	long	j;
+
+	if (a->size <= 1)
+		return (0,0);
+	mistakes = 0;
+	total_pairs = 0;
+	i = 0;
+	while (i < a->size - 1)
+	{
+		j = i + 1;
+		while (j < a->size)
+		{
+			total_pairs += 1;
+			if (a->data[i] > a->data[j])
+				mistakes += 1;
+			j++;
+		}
+		i++;
+	}
+	return (mistakes / total_pairs);
+}
