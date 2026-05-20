2165472d40a921d940f7edd3d59fc9c783c683ed
Kayan Silva Bezerra <kbezerra@c1r6s3.42lisboa.com>
2026-05-20 12:33:25 +0100

    swap

--- swap.c
+++ swap.c
@@ -14,15 +14,28 @@
 
 void	sa(t_stack *a)
 {
+	int temp;
 
+	if (!a || a->top < 1)
+		return ;
+	temp = a->data[a->top];
+	a->data[a->top] = a->data[a->top - 1];
+	a->data[a->top - 1] = temp;
 }
 
 void	sb(t_stack *b)
 {
+	int temp;
 
+	if (!b || b->top < 1)
+		return ;
+	temp = b->data[b->top];
+	b->data[b->top] = b->data[b->top - 1];
+	b->data[b->top - 1] = temp;
 }
 
 void	ss(t_stack *a, t_stack *b)
 {
-	
+	sa(a);
+	sb(b);
 }
