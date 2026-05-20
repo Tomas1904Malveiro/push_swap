5c0a2831bd415d4eb39b4821507ffcfcc381be61
Tomas1904Malveiro <148541225+Tomas1904Malveiro@users.noreply.github.com>
2026-05-20 14:35:30 +0100

    Add files via upload

--- compute_disorder.c
+++ compute_disorder.c
@@ -6,7 +6,7 @@
 /*   By: tochaves <tochaves@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 14:34:27 by kbezerra          #+#    #+#             */
-/*   Updated: 2026/05/19 16:11:43 by tochaves         ###   ########.fr       */
+/*   Updated: 2026/05/20 14:17:07 by tochaves         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@ long	compute_disorder(t_stack *a)
 	long	j;
 
 	if (a->size <= 1)
-		return (0, 0);
+		return (0);
 	mistakes = 0;
 	total_pairs = 0;
 	i = 0;
-	while (i < a->size - 1)
+	while (i <= a->top - 1)
 	{
 		j = i + 1;
-		while (j < a->size)
+		while (j <= a->top)
 		{
 			total_pairs += 1;
 			if (a->data[i] > a->data[j])
@@ -36,5 +36,5 @@ long	compute_disorder(t_stack *a)
 		}
 		i++;
 	}
-	return (mistakes / total_pairs);
+	return (mistakes * 100 / total_pairs);
 }
